754071077251e0784af84a7f1d78b61f019d87b5
alex <apocthiel@gmail.com>
2018-09-17 22:35:03 -0700

    Fixed freeze_tf_ckpt.py for a more modern tensorflow model

--- easy_ml/freeze_tf_ckpt.py
+++ easy_ml/freeze_tf_ckpt.py
@@ -38,28 +38,38 @@ def freeze_graph(model_path, output_node_names):
 
     # We start a session using a temporary fresh Graph
     with tf.Session(graph=tf.Graph()) as sess:
-        # We import the meta graph in the current default Graph
-        saver = tf.train.import_meta_graph(str(meta_file), clear_devices=clear_devices)
+        # Having this line makes the next line not fail (???)
+        # From: https://github.com/tensorflow/tensorflow/issues/21024
+        tf.contrib.resampler
 
-        # We restore the weights
-        saver.restore(sess, model_path)
+        # We import the meta graph in the current default Graph
+        saver = tf.train.import_meta_graph(str(meta_file),
+                                           clear_devices=clear_devices)
+
 
         # Save a graph summary for easy tensorboard viewing
         save_graph_summary(sess, str(Path(model_path).parent))
 
         # Print information about the loaded model
         ops = [o.name for o in tf.get_default_graph().get_operations()]
-        print("All Operations: \n", ops)
+        # print("All Operations: \n", ops)
+
+
+        # We restore the weights
+        saver.restore(sess, model_path)
 
         if not output_node_names:
-            print("You need to supply the name of a node to --output_node_names.")
-            return -1
+            print(
+                "You need to supply the name of a node to --output_node_names.")
+            exit(-1)
 
         # We use a built-in TF helper to export variables to constants
         output_graph_def = tf.graph_util.convert_variables_to_constants(
             sess,  # The session is used to retrieve the weights
-            tf.get_default_graph().as_graph_def(),  # The graph_def is used to retrieve the nodes
-            output_node_names.split(",")  # The output node names are used to select the usefull nodes
+            tf.get_default_graph().as_graph_def(),
+            # The graph_def is used to retrieve the nodes
+            output_node_names.split(",")
+            # The output node names are used to select the usefull nodes
         )
 
         # Finally we serialize and dump the output graph to the filesystem
@@ -72,7 +82,8 @@ def freeze_graph(model_path, output_node_names):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", type=str, required=True, help="Model folder to export")
+    parser.add_argument("--model_path", type=str, required=True,
+                        help="Model folder to export")
     parser.add_argument("--output_node_names", type=str, default="",
                         help="The name of the output nodes, comma separated.")
     args = parser.parse_args()
