ffa5858806a7c3dc16698c4cca6f9ebf75a88917
alex <alex.thiel@dilililabs.com>
2019-04-07 17:30:18 -0700

    More improvements to the reid scripts

--- dataset_utils/person_reid/pipa/resize_images.py
+++ dataset_utils/person_reid/pipa/resize_images.py
@@ -13,18 +13,25 @@ def resize_all(folder_path: Path, image_size):
         for image_path in dir.iterdir():
             if not image_path.is_file():
                 continue
-            if ".DS_STORE" in str(image_path):
-                continue
 
             img = cv2.imread(str(image_path))
+            if img is None:
+                print(f"Removing {image_path}, not a valid image!")
+                os.remove(image_path)
+                continue
+
+            if list(img.shape)[:2] == image_size:
+                print("Skipping, already correct size")
+                continue
+
             img = cv2.resize(img, tuple(image_size))
             cv2.imwrite(str(image_path), img)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Resize all images")
-    parser.add_argument('-f', '--folder_path', type=Path)
-    parser.add_argument('-i', '--image_size', default=[160, 160],
+    parser.add_argument('-f', '--folder_path', type=Path, required=True)
+    parser.add_argument('-i', '--image_size', default=[180, 180],
                         type=int, nargs="+")
     args = parser.parse_args()
     resize_all(folder_path=args.folder_path,
